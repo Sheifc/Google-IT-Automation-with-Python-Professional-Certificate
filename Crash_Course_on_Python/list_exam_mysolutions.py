a8be53174264fa1dc67528eedb49c33c44a0c2eb
SFC <115345487+Sheifc@users.noreply.github.com>
2023-08-29 18:46:55 +0200

    Add files via upload

--- Crash_Course_on_Python/list_exam_mysolutions.py
+++ Crash_Course_on_Python/list_exam_mysolutions.py
@@ -15,7 +15,7 @@ for element in filenames:
     if element.endswith(".hpp"):
         new = element.replace(".hpp", ".h")
         newfilenames.append(new)
-	else:
+    else:
         newfilenames.append(element)
 print(newfilenames)
 # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
@@ -28,18 +28,30 @@ For example, python ends up as ythonpay.
 """
 
 def pig_latin(text):
-  say = ""
-  # Separate the text into words
-  words = text.split()
-  for word in words:
-    # Create the pig latin word and add it to the list
-    pig = word[1:] + word[0] + "ay"
-    say += pig + " "
+    say = ""
+    # Separate the text into words
+    words = text.split()
+    for word in words:
+        # Create the pig latin word and add it to the list
+        pig = word[1:] + word[0] + "ay"
+        say += pig + " "
     # Turn the list back into a phrase
-  return say
+    return say
 
 print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
 print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+"""
+Which list method can be used to add a new element to a list at a specified index position?
+
+list.insert(index, x)
+"""
+
+"""
+Tuples and lists are very similar types of sequences. What is the main thing that makes a tuple different from a list?
+
+A tuple is immutable
+"""
 
 """
 The group_list function accepts a group name and a list of members,
@@ -49,18 +61,26 @@ Fill in the gaps in this function to do that.
 """
 
 def group_list(group, users):
-  members = group + ": " + ", ".join(users)
-  return members
+    members = group + ": " + ", ".join(users)
+    return members
 
 print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
 print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
 print(group_list("Users", "")) # Should be "Users:"
 
-The guest_list function reads in a list of tuples with the name, age, and profession of each party guest, and prints the sentence "Guest is X years old and works as __" for each one. For example, guest_list (('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out: Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old and works as Engineer. Fill in the gaps in this function to do that.
+"""
+Output:
+Marketing: Mike, Karen, Jake, Tasha
+Engineering: Kim, Jay, Tom
+Users:
+"""
 
+"""
+The guest_list function reads in a list of tuples with the name, age, and profession of each party guest, and prints the sentence "Guest is X years old and works as __" for each one. For example, guest_list (('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out: Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old and works as Engineer. Fill in the gaps in this function to do that.
+"""
 def guest_list(guests):
     for element in guests:
-        name, year, occupation = element
+	name, year, occupation = element
 	print("{} is {} years old and works as {}".format(name, year, occupation))
 
 guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])

391dbfc47a3279a6e7ceba7b7164b567b29dbd9c
SFC <115345487+Sheifc@users.noreply.github.com>
2023-08-29 18:41:03 +0200

    Update list_exam_mysolutions.py

--- Crash_Course_on_Python/list_exam_mysolutions.py
+++ Crash_Course_on_Python/list_exam_mysolutions.py
@@ -59,9 +59,9 @@ print(group_list("Users", "")) # Should be "Users:"
 The guest_list function reads in a list of tuples with the name, age, and profession of each party guest, and prints the sentence "Guest is X years old and works as __" for each one. For example, guest_list (('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out: Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old and works as Engineer. Fill in the gaps in this function to do that.
 
 def guest_list(guests):
-	for element in guests:
-		name, year, occupation = element
-		print("{} is {} years old and works as {}".format(name, year, occupation))
+    for element in guests:
+        name, year, occupation = element
+	print("{} is {} years old and works as {}".format(name, year, occupation))
 
 guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
 

8b2bc587261128b1ed7489ba68c2d986138ac71a
SFC <115345487+Sheifc@users.noreply.github.com>
2023-08-30 00:31:48 +0200

    Update strings_lists_dicts_exam_mysolutions.py

--- Crash_Course_on_Python/strings_lists_dicts_exam_mysolutions.py
+++ Crash_Course_on_Python/strings_lists_dicts_exam_mysolutions.py
@@ -2,9 +2,10 @@
 
 """
 Fill in the blanks to complete the "confirm_length" function.
-This function should return how many characters a string contains as long as it has one or more characters,
-otherwise it will return 0.
-Complete the string operations needed in this function so that input like "Monday" will produce the output "6".
+This function should return how many characters a string contains as long as it has one 
+or more characters, otherwise it will return 0.
+Complete the string operations needed in this function so that input like "Monday" 
+will produce the output "6".
 """
 
 def confirm_length(word):
